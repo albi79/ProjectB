858cc66b66305908c7f6dc475724fd0d10eeae50
nisatutucu <0999289@hr.nl>
2021-03-19 19:27:47 +0100

    Registreer Naam en Wachtwoord. Wordt geschreven naar een json file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft;
+using Newtonsoft.Json;
 
 namespace ProjectTest
 {
@@ -6,7 +10,31 @@ namespace ProjectTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo allemaal");
+            Console.WriteLine("Gebruikersnaam:");
+            string Gebruikersnaam = Console.ReadLine();
+
+            Console.WriteLine("Wachtwoord:");
+            string Wachtwoord = Console.ReadLine();
+
+            Dictionary<string, string> Gebruikers = new Dictionary<string, string>
+            {
+                { "Gebruikersnaam", Gebruikersnaam },
+                { "Wachtwoord", Wachtwoord },
+            };
+
+            string json = JsonConvert.SerializeObject(Gebruikers, Formatting.Indented);
+            string path = @"Gebruikers.json";
+
+            //if (File.Exists(path))
+            //{
+            //    File.Delete(path);
+            //}
+
+            using (var tw = new StreamWriter(path, true))
+            {
+                tw.WriteLine(json.ToString());
+                tw.Close();
+            }
         }
     }
 }
