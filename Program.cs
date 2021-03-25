d5c30ffc994997fb24a7136cb7b827042233939c
nisatutucu <0999289@hr.nl>
2021-03-25 21:35:44 +0100

    Registreren formulier begin

--- Program.cs
+++ Program.cs
@@ -4,37 +4,13 @@ using System.IO;
 using Newtonsoft;
 using Newtonsoft.Json;
 
-namespace ProjectTest
+namespace ProjectB
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gebruikersnaam:");
-            string Gebruikersnaam = Console.ReadLine();
-
-            Console.WriteLine("Wachtwoord:");
-            string Wachtwoord = Console.ReadLine();
-
-            Dictionary<string, string> Gebruikers = new Dictionary<string, string>
-            {
-                { "Gebruikersnaam:", Gebruikersnaam },
-                { "Wachtwoord:", Wachtwoord },
-            };
-
-            string json = JsonConvert.SerializeObject(Gebruikers, Formatting.Indented);
-            string path = @"Gebruikers.json";
-
-            //if (File.Exists(path))
-            //{
-            //    File.Delete(path);
-            //}
-
-            using (var tw = new StreamWriter(path, true))
-            {
-                tw.WriteLine(json.ToString());
-                tw.Close();
-            }
+            Registreren.registreren();
         }
     }
 }
---
+++ Registreren.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace ProjectB
+{
+    class Registreren
+    {
+        public static void registreren()
+        {
+            Console.WriteLine("Naam:");
+            string naam = Console.ReadLine();
+
+            Console.WriteLine("Tussenvoegsel:");
+            string tussenvoegsel = Console.ReadLine();
+
+            Console.WriteLine("Achternaam:");
+            string achternaam = Console.ReadLine();
+
+            Console.WriteLine("Geboortedatum:");
+            string geboortedatum = Console.ReadLine();
+
+            Console.WriteLine("E-mail:");
+            string email = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("E-mail bevestiging:");
+            string email2 = Console.ReadLine();
+
+            Console.WriteLine("Gebruikersnaam:");
+            string gebruikersnaam = Console.ReadLine();
+
+            Console.WriteLine("Wachtwoord:");
+            string wachtwoord = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Wachtwoord bevestiging:");
+            string wachtwoord2 = Console.ReadLine();
+
+            Console.Clear();
+
+            Dictionary<string, string> Gebruikers = new Dictionary<string, string>
+            {
+                { "Naam:", naam },
+                { "Tussenvoegsel:", tussenvoegsel },
+                { "Achternaam:", achternaam },
+                { "Geboortedatum:", geboortedatum },
+                { "E-mail:", email },
+                { "E-mail bevestiging:", email2 },
+                { "Gebruikersnaam:", gebruikersnaam },
+                { "Wachtwoord:", wachtwoord },
+                { "Wachtwoord bevestiging:", wachtwoord2 },
+            };
+
+            string json = JsonConvert.SerializeObject(Gebruikers, Formatting.Indented);
+            string path = @"Gebruikers.json";
+
+            //if (File.Exists(path))
+            //{
+            //    File.Delete(path);
+            //}
+
+            using (var tw = new StreamWriter(path, true))
+            {
+                tw.WriteLine(json.ToString());
+                tw.Close();
+            }
+        }
+    }
+}
