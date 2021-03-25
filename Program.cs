e4a7e7074d908c62463657b959850e3ffb7eff34
dkbj22 <dkbj22@gmail.com>
2021-03-25 21:55:01 +0100

    login beginnetje

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+//using Newtonsoft;
 
-namespace ProjectTest
+namespace ProjectB
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Project B");
+            login();
+        }
+
+        static void login()
+        {
+            Console.Clear();
+
+            Console.Write("Gebruikersnaam: ");
+            string UserName = Console.ReadLine();
+
+            Console.Write("Wachtwoord: ");
+            string PassWord = Console.ReadLine();
+            Console.Clear();
+
+            string[] gebruiker = { UserName, PassWord };
+
+            // if gebruiker is bekend -> MenuScherm
+
+            // else
+            Console.WriteLine("login klopt niet \n\nKlik R voor registreren\n\nKlik C voor Continue zonder inloggen\n\nKlik Enter voor opnieuw inloggen");
+
+            var key = Console.ReadKey();
+
+            // if (key == r) -> Naar registreer scherm
+
+            // else if (key == c) -> naar menu scherm
+
+            // else -> login()
         }
     }
 }
