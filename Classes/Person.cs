8c31ebe1371b8aa8b21f993b94395b2eabd2fa0f
nisatutucu <0999289@hr.nl>
2021-03-30 11:52:04 +0200

    feedback met docenten

--- Classes/Person.cs
+++ Classes/Person.cs
@@ -3,12 +3,12 @@ namespace ProjectB.Classes
 {
     public class Person
     {
-        public string naam { get; set; }
-        public string tussenvoegsel { get; set; }
-        public string achternaam { get; set; }
-        public string geboortedatum { get; set; }
-        public string email { get; set; }
-        public string gebruikersnaam { get; set; }
-        public string wachtwoord { get; set; }
+        public string naam { get; set; } // alleen letters - verplicht
+        public string tussenvoegsel { get; set; } // alleen letters - optioneel
+        public string achternaam { get; set; } // alleen letters - verplicht
+        public string geboortedatum { get; set; } // datum maken - verplicht
+        public string email { get; set; } //mail - verplicht
+        public string gebruikersnaam { get; set; } //mail - verplicht
+        public string wachtwoord { get; set; } //wachtwoord eisen toevoegen - verplicht 
     }
 }
