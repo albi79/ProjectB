006e420ebfc3548fbfe0f734150fac46eed5a0ea
Dong Wan Kang <1018910@hr.nl>
2021-04-06 00:13:35 +0200

    Filmprogramma toevoegen en verwijderen is AF en er is daarvoor een Film class gemaakt die net als Person.cs werkt

--- pages/FilmprogrammaBeheren.cs
+++ pages/FilmprogrammaBeheren.cs
@@ -11,28 +11,60 @@ namespace ProjectB.pages
 {
     class FilmprogrammaBeheren
     {
+        public static List<Film> films = new List<Film>(); 
+
         public static void filmprogrammaBeheren()
         {
-            List<string> films = new List<string>();
-
+            Console.Clear();
             Console.WriteLine("1. Film toevoegen");
             Console.WriteLine("2. Film verwijderen");
+            Console.WriteLine("Dit zijn de huidige films die in het programma voortkomen:\n(Titel - Categorie - Leeftijd)\n-------------------------------------------");
 
-            Console.WriteLine(films);
+            //loop door de lijst
+            foreach (Film filmItem in films)
+            {
+                //consolewriteline voor elke item in de list
+                Console.WriteLine(filmItem.Titel + " - " + filmItem.Categorie + " - " + filmItem.Leeftijd);
+            }
 
             string gegeven = Beheer.Input("");
 
             if(gegeven == "1")
             {
-                films.Add(Beheer.Input("Welke film wilt u toevoegen: "));
+                Console.Clear();
+                string nTitel = Beheer.Input("Wat is de titel van de nieuwe film?\n");
+                string nCategorie = Beheer.Input("Wat is de categorie van de nieuwe film?\n");
+                int nLeeftijd = Convert.ToInt32(Beheer.Input("Wat is de minimum leeftijd van de nieuwe film?\n"));
+
+                //TODO: FOUTMELDING
+
+                Film nieuweFilm = new Film
+                {
+                    Titel = nTitel,
+                    Categorie = nCategorie,
+                    Leeftijd = nLeeftijd
+                };
+
+                films.Add(nieuweFilm);
                 FilmprogrammaBeheren.filmprogrammaBeheren();
             }
 
             else if(gegeven == "2")
             {
-                Console.WriteLine(films);
-                Console.WriteLine($"Film 1 - {films[0]}");
-                films.RemoveAt(Convert.ToInt32(Beheer.Input(": ")));
+                Console.Clear();
+
+                string nTitel = Beheer.Input("Welke film wilt u verwijderen? (VOER EXACT TITEL IN) \n");
+
+                foreach (Film filmItem in films)
+                {
+                    if (nTitel == filmItem.Titel)
+                    {
+                        films.Remove(filmItem);
+                        break;
+                    }
+                }
+
+                FilmprogrammaBeheren.filmprogrammaBeheren();
             }
         }
     }
